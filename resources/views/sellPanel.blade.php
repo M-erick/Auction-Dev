b779219dc40121238c43e8827469f5f0c4c1f7fd
eric <61746940+M-erick@users.noreply.github.com>
2022-02-27 22:41:02 +0300

    display the user fetched data in table.

--- resources/views/sellPanel.blade.php
+++ resources/views/sellPanel.blade.php
@@ -4,46 +4,20 @@
     <!-- component -->
     <div class="bg-gray-50 min-h-screen pb-3  mt-28 ">
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 md:m-2 md:gap-x-10 xl-grid-cols-4 gap-y-3 gap-x-6 ">
-
-            <div class="container bg-white  w-full border-b border-gray-300 ">
-                {{-- <img src="images/dollars.webp" alt="" class=" w-full"> --}}
+            <div class="container   bg-white min-h-full w-full   border-t border-b border-gray-300 ">
                 <div class="p-6">
-                    {{-- Display if share is succcessful --}}
-                    @foreach ($timer as $data)
-                    @if ($data->name == auth()->user()->name)
-
-
-
-                    <p>Days :{{ ucwords($data->name) }}</p>
-                    <p>Days :{{ date('jS M Y', strtotime($data->created_at)) }}</p>
-
-
-                    <p>Days :{{ $data->days }}</p>
-
-
-
-                    <p>Amount placed :{{ $data->amount }}</p>
-                    {{-- <p>Date  placed :{{ $temp[0] }}</p>
-                    <p>Date  placed :{{ $temp[1] }}</p> --}}
-
-
-
-                    {{-- <p>Amount  placed :{{ $data->created_at }}</p> --}}
-
-
-                @endif
-
-                    @endforeach
-                    {{-- <h1 class="md:text-1xl text-xl hover:text-indigo-600 transition duration-200  font-bold text-gray-900 ">
+                    <h1 class="md:text-1xl text-xl hover:text-indigo-600 transition duration-200  font-bold text-gray-900 ">
                         This is Amazing for people to visit.</h1>
                     <p class="text-gray-700 my-2 ">Lorem ipsum dolor sit amet consectetur adipisicing elit.
                         Praesentium quis.</p>
                     <p class="text-gray-700 my-2 "> Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
                         deserunt, culpa mollitia quisquam minus
                         vel architecto pariatur maiores eligendi aperiam nobis numquam consequuntur sint modi, eaque
-                        adipisci animi distinctio iusto.</p> --}}
+                        adipisci animi distinctio iusto.</p>
                 </div>
             </div>
+
+
             {{-- card two --}}
 
             <div class="container w-full   bg-white border-t border-b border-gray-300 ">
@@ -94,29 +68,61 @@
                 </div>
             </div>
 
-            <div class="container   bg-white min-h-full w-full   border-t border-b border-gray-300 ">
+            <div class="container bg-white  w-full border-b border-gray-300 ">
+                {{-- <img src="images/dollars.webp" alt="" class=" w-full"> --}}
                 <div class="p-6">
-                    <h1 class="md:text-1xl text-xl hover:text-indigo-600 transition duration-200  font-bold text-gray-900 ">
+
+                    {{-- Display if share is succcessful --}}
+                    @foreach ($timer as $data)
+                    @if ($data->name == auth()->user()->name)
+
+
+
+                    <p>Days :{{ ucwords($data->name) }}</p>
+                    <p>Days :{{ date('jS M Y', strtotime($data->created_at)) }}</p>
+
+
+                    <p>Days :{{ $data->days }}</p>
+
+
+
+                    <p>Amount placed :{{ $data->amount }}</p>
+                    {{-- <p>Date  placed :{{ $temp[0] }}</p>
+                    <p>Date  placed :{{ $temp[1] }}</p> --}}
+
+
+
+                    {{-- <p>Amount  placed :{{ $data->created_at }}</p> --}}
+
+
+                @endif
+
+                    @endforeach
+                    {{-- <h1 class="md:text-1xl text-xl hover:text-indigo-600 transition duration-200  font-bold text-gray-900 ">
                         This is Amazing for people to visit.</h1>
                     <p class="text-gray-700 my-2 ">Lorem ipsum dolor sit amet consectetur adipisicing elit.
                         Praesentium quis.</p>
                     <p class="text-gray-700 my-2 "> Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
                         deserunt, culpa mollitia quisquam minus
                         vel architecto pariatur maiores eligendi aperiam nobis numquam consequuntur sint modi, eaque
-                        adipisci animi distinctio iusto.</p>
+                        adipisci animi distinctio iusto.</p> --}}
                 </div>
             </div>
             {{-- last card --}}
             <div class="container   min-h-full w-full   bg-white border-t border-b border-gray-300 ">
                 <div class="p-6">
-                    <h1 class="md:text-1xl text-xl hover:text-indigo-600 transition duration-200  font-bold text-gray-900 ">
+                    {{-- <x-sellsharestable/> --}}
+                    @include('showBoughtShare')
+
+
+                    {{-- <h1 class="md:text-1xl text-xl hover:text-indigo-600 transition duration-200  font-bold text-gray-900 ">
                         DIsplay currently bought shares</h1>
                     <p class="text-gray-700 my-2 ">Lorem ipsum dolor sit amet consectetur adipisicing elit.
                         Praesentium quis.</p>
                     <p class="text-gray-700 my-2 "> Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
                         deserunt, culpa mollitia quisquam minus
                         vel architecto pariatur maiores eligendi aperiam nobis numquam consequuntur sint modi, eaque
-                        adipisci animi distinctio iusto.</p>
+                        adipisci animi distinctio iusto.</p> --}}
                 </div>
             </div>
 
